3b0cc96edf4eba21067750be81a7c012d1eb50ca
Antoine-Alexandr Leytens <aa.leytens@gmail.com>
2024-05-24 17:15:59 +0200

    adding home

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { NavbarComponent } from './components/navbar/navbar.component'
+import { HomeComponent } from './pages/home/home.component'
+import { WebProjectsComponent } from './pages/web-projects/web-projects.component'
+import { ContactComponent } from './pages/contact/contact.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, WebProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

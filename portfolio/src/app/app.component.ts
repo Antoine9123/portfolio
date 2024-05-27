1fc948a31964104b25ca9054f627b6d6d6559598
Antoine-Alexandr Leytens <aa.leytens@gmail.com>
2024-05-27 10:19:14 +0200

    adding menu link working

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 import { NavbarComponent } from './components/navbar/navbar.component'
 import { HomeComponent } from './pages/home/home.component'
--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { WebProjectsComponent } from './pages/web-projects/web-projects.component';
+
+export const routes: Routes = [
+    {
+        path: 'home', component: HomeComponent
+    },
+    {
+        path: 'contact', component: ContactComponent
+    },
+    {
+        path: 'webprojects', component: WebProjectsComponent
+    }
+];
